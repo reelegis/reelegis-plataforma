815b9a4b536cecf2a4118b42eccda3cfc43ab2ab
reelegis <110792099+reelegis@users.noreply.github.com>
2022-08-19 10:13:42 -0300

    Update reelegis_app.py

--- reelegis_app.py
+++ reelegis_app.py
@@ -68,8 +68,8 @@ if pol_part == 'Político':
         parlamentar_do_estado = f_par2['nomeUrna'].unique()
         parlamentar_do_estado = np.append(parlamentar_do_estado, '')
         parlamentar_do_estado.sort()
-        st.subheader('Qual político você gostaria de visualizar?')
-        escolha_parlamentar_do_estado = st.selectbox("Selecione o Candidato", parlamentar_do_estado)
+        st.subheader('Qual Parlamentar você gostaria de visualizar?')
+        escolha_parlamentar_do_estado = st.selectbox("Selecione o Parlamentar", parlamentar_do_estado)
         #st.error(f'Caso você não encontre o/a Deputado/a do seu estado, isso é devido ao fato dele/a não estar concorrendo à reeleição, ou não apresentou propostas até o período de nossa coleta (18/07/2022).')
         if escolha_parlamentar_do_estado != '':
             f_par23 = f_par2.loc[f_par2.nomeUrna == escolha_parlamentar_do_estado, :]
