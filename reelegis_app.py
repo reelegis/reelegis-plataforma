c9f9f4ab47f4137ac44acd7359694a85e156031f
reelegis <110792099+reelegis@users.noreply.github.com>
2022-09-05 14:48:10 -0300

    Update reelegis_app.py

--- reelegis_app.py
+++ reelegis_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
-components.html('<label>Carregando a plataforma e atualizando dados <progress></progress></label>')
 
 import pandas as pd
 import numpy as np
